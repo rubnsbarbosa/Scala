0ef8dcb291f1c8958c93c1414060831d85550725
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-06-29 06:53:00 -0300

    string interpolation

---
+++ src/main/scala/Variables/StringInterpolation.scala
@@ -0,0 +1,18 @@
+package Variables
+
+object StringInterpolation {
+
+  def main(args: Array[String]) {
+    val name = "Scala"
+    val year = 2020
+
+    println(name + " in " + year + ".")
+    println(s"$name in $year.")
+    println(f"$name%s in $year%d.")
+
+    println(s"Hello \nScala")
+    //println(raw"Hello \nScala")
+
+  }
+
+}
