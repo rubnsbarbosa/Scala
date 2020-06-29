54df2505dc5938b14cd2baaa5383657e90431566
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-06-29 07:28:27 -0300

    if and else

---
+++ src/main/scala/Statements/ifandelse.scala
@@ -0,0 +1,19 @@
+package Statements
+
+object ifandelse {
+  def main(args: Array[String]) {
+    val x: Int = 20;
+    var res = "";
+
+    if (x == 20) {
+      res = "x == 20";
+    } else {
+      res = "x != 20";
+    }
+    println(res);
+
+    val res2 = if (x != 20) "x == 20" else "x != 20";
+    println(res2);
+
+  }
+}
